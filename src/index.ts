7df2e1ccabdaf10fca5665b5ce1d1dca23746b14
Louis Orleans <louis@orleans.io>
2024-07-10 15:02:02 -0700

    ✨ add bin

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,29 @@ import { stringify } from 'csv-stringify/sync';
 import { parseMarkdown } from './parse.js';
 import { generateJiraConfig } from './jira-config.js';
 
-const input_filename = process.argv[2]!;
-const input_filename_base = basename(input_filename, '.md');
+export async function main() {
 
-await Promise.all([
-  readFile(input_filename, { encoding: 'utf8' })
-    .then(parseMarkdown)
-    .then((issues) =>
-      stringify(issues, {
-        header: true,
-        objectMode: true,
-      }),
-    )
-    .then((csv) => writeFile(`${input_filename_base}.csv`, csv)),
 
-  generateJiraConfig()
-    .then((config) =>
-      // We use JSON.stringify instead of Effect's stringify because we want the output formatted
-      JSON.stringify(config, null, 2),
-    )
-    .then((json) => writeFile(`${input_filename_base}.json`, json)),
-]).then(() => console.log('Created CSV & config JSON. Have fun with Jira 😉'));
+  const input_filename_base = basename(options.issues_file, '.md');
+
+  await Promise.all([
+    readFile(options.issues_file, { encoding: 'utf8' })
+      .then(parseMarkdown)
+      .then((issues) =>
+        stringify(issues, {
+          header: true,
+          objectMode: true,
+        }),
+      )
+      .then((csv) => writeFile(`${input_filename_base}.csv`, csv)),
+
+    generateJiraConfig()
+      .then((config) =>
+        // We use JSON.stringify instead of Effect's stringify because we want the output formatted
+        JSON.stringify(config, null, 2),
+      )
+      .then((json) => writeFile(`${input_filename_base}.json`, json)),
+  ]).then(() =>
+    console.log('Created CSV & config JSON. Have fun with Jira 😉'),
+  );
+}
---
+++ src/jira-issue-generator.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env -S node --trace-uncaught --enable-source-maps
+
+import { main } from './index.js';
+
+void main();
