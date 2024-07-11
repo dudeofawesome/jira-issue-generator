ce2e390fb808e9da2e425b20b9a14c46fe72f0fb
Louis Orleans <louis@orleans.io>
2024-07-11 13:12:29 -0700

    🐛✨ fix list spacing & support ordered lists

--- src/utils.ts
+++ src/utils.ts
@@ -3,25 +3,40 @@
  * https://jira.atlassian.com/secure/WikiRendererHelpAction.jspa?section=all
  */
 export function MarkdownToMarkup(markdown: string): string {
-  // TODO: support ordered lists
   // TODO: support tables
   // TODO: support block quotes
   // TODO: support struck text
   // TODO: support multi-level lists
   // TODO: support nested styling (this might require an AST. `markdown-it` can generate one)
-  return markdown
-    .replaceAll(
-      /^(#+)\s+/gimu,
-      (_, group1: string) => `h${group1.split('#').length - 1}. `,
-    )
-    .replaceAll(
-      /```(?<lang>\w+)\n(?<code>[^`].+?[^`])\n```/giu,
-      (_, lang: string, code: string) => `{code:${lang}}\n${code}\n{code}`,
-    )
-    .replaceAll(/`([^`].+?[^`])`/giu, (_, group1: string) => `{{${group1}}}`)
-    .replaceAll(/\*\*(.+?)\*\*/giu, (_, group1: string) => `*${group1}*`)
-    .replaceAll(
-      /\[(?<text>.+?)\]\((?<url>.+?)\)/giu,
-      (_, text: string, url: string) => `[${text}|${url}]`,
-    );
+  return (
+    markdown
+      // Headers
+      .replaceAll(
+        /^(#+)\s+/gimu,
+        (_, group1: string) => `h${group1.split('#').length - 1}. `,
+      )
+      // Code blocks
+      .replaceAll(
+        /```(?<lang>\w+)\n(?<code>[^`].+?[^`])\n```/giu,
+        (_, lang: string, code: string) => `{code:${lang}}\n${code}\n{code}`,
+      )
+      // Code
+      .replaceAll(/`([^`].+?[^`])`/giu, (_, group1: string) => `{{${group1}}}`)
+      // Bold
+      .replaceAll(/\*\*(.+?)\*\*/giu, (_, group1: string) => `*${group1}*`)
+      // Links
+      .replaceAll(
+        /\[(?<text>.+?)\]\((?<url>.+?)\)/giu,
+        (_, text: string, url: string) => `[${text}|${url}]`,
+      )
+      // Lists
+      .replaceAll(
+        /^(?<indent>\s*)(?<list>-|\*|\+|(?:\d+\.))\s+(?<item>.+)$/gimu,
+        (_, indent: string, list: '-' | '*' | `${number}.`, item: string) =>
+          // TODO: the `4` here is an assumption about the amount of indent in Markdown
+          `${(['*', '-', '+'].includes(list) ? '*' : '#').repeat(
+            indent.length / 4 + 1,
+          )} ${item}`,
+      )
+  );
 }
