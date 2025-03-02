2a855b09df16376aa1f58aeffceef1789d150e02
Louis Orleans <louis@orleans.io>
2025-03-02 01:43:15 -0800

    🐛 fix scripts

---
+++ packages/jira-issue-generator/src/parse.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+
+import { parseFrontmatter } from './parse.js';
+
+describe('parse', () => {
+  describe(parseFrontmatter.name, () => {
+    it('no test', () => {
+      expect(true).toBe(true);
+    });
+  });
+});
